91e3815d60e78ea520ce1d5502d4e72fc5bb2607
Renan Rhoden <renanrhoden@gmail.com>
2018-12-01 16:37:07 -0200

    removing winmm

---
+++ src/shader_fragment.glsl
@@ -0,0 +1,302 @@
+#version 330 core
+
+// Atributos de fragmentos recebidos como entrada ("in") pelo Fragment Shader.
+// Neste exemplo, este atributo foi gerado pelo rasterizador como a
+// interpolação da cor de cada vértice, definidas em "shader_vertex.glsl" e
+// "main.cpp".
+in vec4 position_world;
+in vec4 normal;
+
+// Posição do vértice atual no sistema de coordenadas local do modelo.
+in vec4 position_model;
+
+// Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
+in vec2 texcoords;
+
+// Matrizes computadas no código C++ e enviadas para a GPU
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+// Identificador que define qual objeto está sendo desenhado no momento
+#define SPHERE 0
+#define BUNNY  1
+#define PLANE  2
+#define COW 3
+#define FENCE 4
+#define HOUSE 5
+#define HOUSE2 6
+#define HOUSE3 7
+#define HOUSE4 8
+#define GAMEOVER 9
+uniform int object_id;
+
+// Parâmetros da axis-aligned bounding box (AABB) do modelo
+uniform vec4 bbox_min;
+uniform vec4 bbox_max;
+
+// Variáveis para acesso das imagens de textura
+uniform sampler2D TextureImage0;
+uniform sampler2D TextureImage1;
+uniform sampler2D TextureImage2;
+uniform sampler2D TextureImage3;
+uniform sampler2D TextureImage4;
+uniform sampler2D TextureImage5;
+uniform sampler2D TextureImage6;
+uniform sampler2D TextureImage7;
+uniform sampler2D TextureImage8;
+uniform sampler2D TextureImage9;
+uniform sampler2D TextureImage10;
+uniform sampler2D TextureImage11;
+
+
+// O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
+out vec3 color;
+
+// Constantes
+#define M_PI   3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
+
+void main()
+{
+    // Obtemos a posição da câmera utilizando a inversa da matriz que define o
+    // sistema de coordenadas da câmera.
+    vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 camera_position = inverse(view) * origin;
+
+    // O fragmento atual é coberto por um ponto que percente à superfície de um
+    // dos objetos virtuais da cena. Este ponto, p, possui uma posição no
+    // sistema de coordenadas global (World coordinates). Esta posição é obtida
+    // através da interpolação, feita pelo rasterizador, da posição de cada
+    // vértice.
+    vec4 p = position_world;
+
+    // Normal do fragmento atual, interpolada pelo rasterizador a partir das
+    // normais de cada vértice.
+    vec4 n = normalize(normal);
+
+    // Vetor que define o sentido da fonte de luz em relação ao ponto atual.
+    vec4 l = normalize(vec4(1.0,1.0,0.0,0.0));
+
+    // Vetor que define o sentido da câmera em relação ao ponto atual.
+    vec4 v = normalize(camera_position - p);
+
+    // Vetor que define o sentido da reflexão especular ideal.
+    vec4 r = normalize(-l + 2*n*(dot(n,l))); // PREENCHA AQUI o vetor de reflexão especular ideal
+
+    // Coordenadas de textura U e V
+    float U = 0.0;
+    float V = 0.0;
+
+    // Parâmetros que definem as propriedades espectrais da superfície
+    vec3 Kdif; // Refletância difusa
+    vec3 Ks; // Refletância especular
+    vec3 Ka; // Refletância ambiente
+    float q; // Expoente especular para o modelo de iluminação de Phong
+
+    if ( object_id == SPHERE )
+    {
+        // PREENCHA AQUI as coordenadas de textura da esfera, computadas com
+        // projeção esférica EM COORDENADAS DO MODELO. Utilize como referência
+        // o slide 139 do documento "Aula_20_e_21_Mapeamento_de_Texturas.pdf".
+        // A esfera que define a projeção deve estar centrada na posição
+        // "bbox_center" definida abaixo.
+
+        // Você deve utilizar:
+        //   função 'length( )' : comprimento Euclidiano de um vetor
+        //   função 'atan( , )' : arcotangente. Veja https://en.wikipedia.org/wiki/Atan2.
+        //   função 'asin( )'   : seno inverso.
+        //   constante M_PI
+        //   variável position_model
+
+        vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
+        vec4 p_vector = position_model - bbox_center;
+
+        float rho = length(p_vector);
+        float theta = atan(position_model.x,position_model.z);
+        float phi = asin(position_model.y/rho);
+
+
+        U = (theta + M_PI)/(2*M_PI);
+        V = (phi + M_PI/2)/(M_PI);
+    }
+    else if ( object_id == BUNNY )
+    {
+        // PREENCHA AQUI as coordenadas de textura do coelho, computadas com
+        // projeção planar XY em COORDENADAS DO MODELO. Utilize como referência
+        // o slide 106 do documento "Aula_20_e_21_Mapeamento_de_Texturas.pdf",
+        // e também use as variáveis min*/max* definidas abaixo para normalizar
+        // as coordenadas de textura U e V dentro do intervalo [0,1]. Para
+        // tanto, veja por exemplo o mapeamento da variável 'p_v' utilizando
+        // 'h' no slide 151 do documento "Aula_20_e_21_Mapeamento_de_Texturas.pdf".
+
+        // PREENCHA AQUI
+        // Propriedades espectrais do coelho
+        Kdif = vec3(0.58, 0.4, 0.1);
+        Ks = vec3(4.0, 4.0, 0.0);
+        Ka = vec3(0.1,0.1,0.1);
+        q = 32.0;
+
+
+
+        float minx = bbox_min.x;
+        float maxx = bbox_max.x;
+
+        float miny = bbox_min.y;
+        float maxy = bbox_max.y;
+
+        float minz = bbox_min.z;
+        float maxz = bbox_max.z;
+
+
+
+        U = (position_model.x - minx)/(maxx - minx);
+        V = (position_model.y - miny)/(maxy - miny);
+    }
+
+    else if ( (object_id == HOUSE) || (object_id == HOUSE2)  || (object_id == HOUSE3)  || (object_id == HOUSE4))
+    {
+
+        Kdif = vec3(0.58, 0.4, 0.1);
+        Ks = vec3(1.0, 1.0, 0.0);
+        Ka = vec3(0.1,0.1,0.1);
+        q = 32.0;
+
+        float minx = bbox_min.x;
+        float maxx = bbox_max.x;
+
+        float miny = bbox_min.y;
+        float maxy = bbox_max.y;
+
+        float minz = bbox_min.z;
+        float maxz = bbox_max.z;
+
+        U = (position_model.x - minx)/(maxx - minx);
+        V = (position_model.y - miny)/(maxy - miny);
+    }
+    else if ( object_id == COW )
+    {
+
+        float minx = bbox_min.x;
+        float maxx = bbox_max.x;
+
+        float miny = bbox_min.y;
+        float maxy = bbox_max.y;
+
+        float minz = bbox_min.z;
+        float maxz = bbox_max.z;
+
+        U = (position_model.x - minx)/(maxx - minx);
+        V = (position_model.y - miny)/(maxy - miny);
+    }
+
+    else if ( object_id == PLANE )
+    {
+        // Coordenadas de textura do plano, obtidas do arquivo OBJ.
+        U = texcoords.x;
+        V = texcoords.y;
+    }
+    else if ( object_id == GAMEOVER )
+    {
+        // Coordenadas de textura do plano, obtidas do arquivo OBJ.
+        U = texcoords.x;
+        V = texcoords.y;
+    }
+    else if ( object_id == FENCE )
+    {
+        float minx = bbox_min.x;
+        float maxx = bbox_max.x;
+
+        float miny = bbox_min.y;
+        float maxy = bbox_max.y;
+
+        float minz = bbox_min.z;
+        float maxz = bbox_max.z;
+
+        U = (position_model.x - minx)/(maxx - minx);
+        V = (position_model.y - miny)/(maxy - miny);
+
+    }
+
+    // Obtemos a refletância difusa a partir da leitura da imagem TextureImage0
+    vec3 Kd0 = texture(TextureImage6, vec2(U,V)).rgb;
+    vec3 Kd1 = texture(TextureImage1, vec2(U,V)).rgb;
+    vec3 Kd2 = texture(TextureImage2, vec2(U,V)).rgb;
+    vec3 Kd3 = texture(TextureImage3, vec2(U,V)).rgb;
+    vec3 Kd4 = texture(TextureImage4, vec2(U,V)).rgb;
+    vec3 Kd_vaca = texture(TextureImage5, vec2(U,V)).rgb;
+    vec3 Kd_casa = texture(TextureImage7,vec2(U,V)).rgb;
+    vec3 Kd_casa2 = texture(TextureImage8,vec2(U,V)).rgb;
+    vec3 Kd_casa3 = texture(TextureImage9,vec2(U,V)).rgb;
+    vec3 Kd_casa4 = texture(TextureImage10,vec2(U,V)).rgb;
+    vec3 Kd_gameover = texture(TextureImage11, vec2(U,V)).rgb;
+
+    // Equação de Iluminação
+
+    // Espectro da fonte de iluminação
+    vec3 I = vec3(1.0,1.0,1.0); // PREENCH AQUI o espectro da fonte de luz
+
+    // Espectro da luz ambiente
+    vec3 Ia = vec3(0.1,0.1,0.0); // PREENCHA AQUI o espectro da luz ambiente
+
+    // Termo difuso utilizando a lei dos cossenos de Lambert
+    vec3 lambert_diffuse_term = Kdif*I*max(0,dot(n,l)); // PREENCHA AQUI o termo difuso de Lambert
+
+    // Termo ambiente
+    vec3 ambient_term = Ka*Ia; // PREENCHA AQUI o termo ambiente
+
+    // Termo especular utilizando o modelo de iluminação de Phong
+    vec3 phong_specular_term  = Ks*I*pow(max(0,dot(r,v)),q); // PREENCH AQUI o termo especular de Phong
+
+    // Cor final do fragmento calculada com uma combinação dos termos difuso,
+    // especular, e ambiente. Veja slide 134 do documento "Aula_17_e_18_Modelos_de_Iluminacao.pdf".
+    //color = lambert_diffuse_term + ambient_term + phong_specular_term;
+
+    float lambert = max(0,dot(n,l));
+
+    if ( object_id == SPHERE )
+    {
+     color = (Kd3 * (lambert + 0.01)) /*+ (Kd1 * (1-lambert))*/;
+    }
+    else if (object_id == BUNNY)
+    {
+    color = (Kd0 * (lambert + 0.01))+ phong_specular_term;
+    }
+    else if (object_id == PLANE)
+    {
+    color = (Kd2 * (lambert + 0.01));
+    }
+    else if (object_id == COW)
+    {
+    color = (Kd_vaca * (lambert + 0.01));
+    }
+    else if (object_id == FENCE)
+    {
+    color = (Kd4 * (lambert + 0.5));
+    }
+    else if (object_id == HOUSE)
+    {
+    color = (Kd_casa * (lambert - 0.185));
+    }
+    else if (object_id == HOUSE2)
+    {
+    color = (Kd_casa2 * (lambert - 0.2));
+    }
+    else if (object_id == HOUSE3)
+    {
+    color = (Kd_casa3 * (lambert - 0.2));
+    }
+    else if (object_id == HOUSE4)
+    {
+    color = (Kd_casa4 * (lambert + 0.2));
+    }
+    else if (object_id == GAMEOVER)
+    {
+    color = (Kd_gameover * (lambert + 0.2));
+    }
+
+
+    // Cor final com correção gamma, considerando monitor sRGB.
+    // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+    color = pow(color, vec3(1.0,1.0,1.0)/2.2);
+}
