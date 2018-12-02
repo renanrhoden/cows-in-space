e0d6d17063767548aa4360e6717bbb861112be26
Renan Rhoden <renanrhoden@gmail.com>
2018-12-02 20:05:33 -0200

    bola bezier

--- src/main.cpp
+++ src/main.cpp
@@ -844,12 +844,12 @@ int main(int argc, char* argv[])
         glUniform1i(object_id_uniform, HEART);
         DrawVirtualObject("heart");
         */
-
+/*
         // Bézier
-        glm::vec3 p1 = glm::vec3(1.5f, 0.0f, -8.0f);
-        glm::vec3 p2 = glm::vec3(-0.5f, 7.0f, -8.0f);
-        glm::vec3 p3 = glm::vec3(-2.0f, 0.0f, -8.0f);
-        glm::vec3 novo_ponto = move_ao_longo_bezier(p1, p2, p2, p3);
+         p1 = glm::vec3(1.5f, 0.0f, -8.0f);
+         p2 = glm::vec3(-0.5f, 7.0f, -8.0f);
+         p3 = glm::vec3(-2.0f, 0.0f, -8.0f);
+        novo_ponto = move_ao_longo_bezier(p1, p2, p2, p3);
         model = Matrix_Translate(novo_ponto.x,novo_ponto.y,novo_ponto.z) * Matrix_Scale(1.0f,1.0f,1.0f);
         glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, BALL);
@@ -863,7 +863,7 @@ int main(int argc, char* argv[])
         model = Matrix_Translate(v_novo_ponto.x,v_novo_ponto.y,v_novo_ponto.z) * Matrix_Scale(vaca_tam_2,vaca_tam_2,vaca_tam_2);
         glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, COW);
-        DrawVirtualObject("cow");
+        DrawVirtualObject("cow"); */
         end_time = clock();
 
         // velocidade da vacaclc
